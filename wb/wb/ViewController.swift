78a693417701311436f05fc0e9a90334e0c52830
lingsu <q25a25q@live.com>
2014-07-27 21:42:38 -0700

    1

---
+++ wb/wb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wb
+//
+//  Created by 吕杏素 on 14-7-27.
+//  Copyright (c) 2014年 吕杏素. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
