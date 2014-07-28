9486f8aa084e5a1754e08af7e988913f8400af3d
lingsu <q25a25q@live.com>
2014-07-27 22:25:20 -0700

    图片

--- wb/wb/AppDelegate.swift
+++ wb/wb/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window=UIWindow()
+        self.window!.frame=UIScreen.mainScreen().bounds
+        
+        self.window!.rootViewController=UITabBarController()
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
